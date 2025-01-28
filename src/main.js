9b1dee062cdb5272838de9c5322164cb927d990e
cyraaryc <bwitten@ucsc.edu>
2025-01-27 19:36:11 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+
+
+//Cyra Witten
+// Rocket Patrol - 
+// 10 hours
+// Display the time remaining (in seconds) on the screen (3), Create a new title screen(3), Implement parallax scrolling for the background (3),
+//Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+//Implement mouse control for player movement and left mouse click to fire (5)
+//Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (5)
+// 29 total, or 26 if the the timer doesnt count twice
+// citations for any sources you used in your code (you do not need to cite Nathan's code or Phaser documentation)
+
 let config = {
     type: Phaser.AUTO,
     width: 1280,
