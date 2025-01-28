51c61f0337859d9bb1684a89b14f1984f04d2771
cyraaryc <bwitten@ucsc.edu>
2025-01-27 19:53:46 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@
 //Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (5)
 // 29 total, or 26 if the the timer doesnt count twice
 // https://opengameart.org/content/space-parallax-background -background //https://opengameart.org/content/space-ship-building-bits-volume-1 -
-attrubution to Redshrike // https://opengameart.org/content/boxy-bold-truetype-font - font
+//attrubution to Redshrike // https://opengameart.org/content/boxy-bold-truetype-font - font
 
 let config = {
     type: Phaser.AUTO,
